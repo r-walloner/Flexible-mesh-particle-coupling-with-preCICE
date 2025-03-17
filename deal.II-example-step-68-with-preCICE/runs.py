21b0c7f35933d33f921a3a83feefc43b4badc6bf
Robin Walloner <wallonerrobin@gmail.com>
2025-03-17 16:48:45 +0100

    Consolidate python tooling for runs into one file

--- deal.II-example-step-68-with-preCICE/runs.py
+++ deal.II-example-step-68-with-preCICE/runs.py
@@ -1,6 +1,11 @@
+"""This file contains utility functions to automate the process of running
+multiple instances (called runs) of the simulation with different parameters.
+The functions in this file are used by the scripts in the `runs` directory to
+generate simulation runs, run them, and postprocess the results."""
+
 import pathlib
 import subprocess
-import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
 
 script_path = pathlib.Path(__file__).resolve()
 
@@ -9,8 +14,103 @@ if not executable_path.is_file():
     raise FileNotFoundError(f"Executable {executable_path} not found")
 
 
+def generate_run(
+    path: pathlib.Path,
+    refinement=4,
+    mapping="rbf-pum-direct",
+    basis_function="compact_polynomial-c6",
+    support_radius=0.5,
+    constraint="consisent",
+):
+    """Generate a new run directory with the given parameters.
+
+    This function creates the directory specified by `path` and writes the
+    configuration files `precice-config.xml` and `parameters.prm` to it. The
+    configuration files are generated with the given parameters."""
+
+    if path.exists():
+        print(
+            f"Not generating run {path.relative_to(script_path.parent)}: Directory already exists"
+        )
+        return
+
+    path.mkdir(parents=True)
+
+    # Write precice-config.xml
+    with open(path / "precice-config.xml", "w") as file:
+        file.write(f"""
+<precice-configuration experimental="true">
+
+  <data:vector name="Velocity" />
+
+  <mesh name="Fluid-Mesh" dimensions="2">
+    <use-data name="Velocity" />
+  </mesh>
+
+  <participant name="Fluid">
+    <provide-mesh name="Fluid-Mesh" />
+    <write-data name="Velocity" mesh="Fluid-Mesh" />
+  </participant>
+
+  <participant name="Particle">
+    <receive-mesh name="Fluid-Mesh" from="Fluid" api-access="true" />
+    <read-data name="Velocity" mesh="Fluid-Mesh" />
+    <mapping:{mapping}
+      direction="read"
+      from="Fluid-Mesh"
+      constraint="{constraint}">
+      {f'<basis-function:{basis_function} support-radius="{support_radius}" />' if mapping == "rbf-pum-direct" else ""}
+    </mapping:{mapping}>
+  </participant>
+
+  <m2n:sockets
+    acceptor="Fluid"
+    connector="Particle"
+    exchange-directory="{path.resolve()}" />
+
+  <coupling-scheme:serial-explicit>
+    <participants first="Fluid" second="Particle" />
+    <max-time value="4.0" />
+    <time-window-size value="0.002" />
+    <exchange data="Velocity" mesh="Fluid-Mesh" from="Fluid" to="Particle" />
+  </coupling-scheme:serial-explicit>
+
+</precice-configuration>
+""")
+
+    # Write parameters.prm
+    with open(path / "parameters.prm", "w") as file:
+        file.write(f"""
+# Listing of Parameters
+# ---------------------
+subsection Particle Tracking Problem
+  # End time of the simulation
+  set Final time                    = 4
+
+  # Refinement level of the fluid domain
+  set Fluid refinement              = {refinement}
+  set Output basename               = {path.name}_
+  set Output directory              = {path.parent.resolve()}/
+
+  # Iteration interval between which output results are written
+  set Output interval               = 10
+
+  # Refinement level of the particle domain
+  set Particle grid refinement      = {refinement}
+
+  # Refinement of the volumetric mesh used to insert the particles
+  set Particle insertion refinement = 3
+
+  # Iteration interval at which the mesh is load balanced
+  set Repartition interval          = 5
+  set Time step                     = 0.002
+  set Velocity degree               = 1
+end
+""")
+
+
 def run(path: pathlib.Path):
-    """Execute one run with the config files in the given directory."""
+    """Execute the run in the given directory."""
     try:
         print(f"Running {path.relative_to(script_path.parent)}")
 
@@ -115,12 +215,12 @@ def run_all(path: pathlib.Path, threads: int = 1):
     """Recursively find all runs in the given directory and execute them.
 
     If threads is greater than 1, runs are executed in parallel using the given
-    number of threads."""
+    number of threads. Else, runs are executed sequentially."""
 
     run_paths = find_runs(path)
 
     if threads > 1:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        with ThreadPoolExecutor(max_workers=threads) as executor:
             executor.map(run, run_paths)
     else:
         for path in run_paths:
