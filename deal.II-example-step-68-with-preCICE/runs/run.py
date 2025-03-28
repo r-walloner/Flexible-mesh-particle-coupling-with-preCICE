ff8abe796a3ced47f3ba7c943899e723a7f9d635
Robin Walloner <wallonerrobin@gmail.com>
2025-03-28 14:43:52 +0100

    Fix run script

--- deal.II-example-step-68-with-preCICE/runs/run.py
+++ deal.II-example-step-68-with-preCICE/runs/run.py
@@ -3,7 +3,7 @@ import pathlib
 
 # Add top-level directory to path
 script_path = pathlib.Path(__file__).resolve()
-sys.path.append(str(script_path.parents[2]))
+sys.path.append(str(script_path.parents[1]))
 from runs import run_all  # noqa: E402
 
 run_all(script_path.parent, threads=12)
