98652c41ce66adf45c7efb8c76f8a0238f470a57
Robin Walloner <wallonerrobin@gmail.com>
2025-03-28 14:22:43 +0100

    Clean up directory structure

---
+++ deal.II-example-step-68-with-preCICE/runs/generate.py
@@ -0,0 +1,51 @@
+import pathlib
+import sys
+
+# Add top-level directory to path
+script_path = pathlib.Path(__file__).resolve()
+sys.path.append(str(script_path.parents[1]))
+from runs import generate_run  # noqa: E402
+
+# Set constant parameters
+constraint = "consistent"
+support_radius = 0.5  # only used for rbf
+
+# Iterate over variable parameters
+for method in [
+    "euler_explicit",
+    # "euler_implicit",
+    # "trapezoidal",
+]:
+    for mapping in [
+        "nearest-neighbor",
+        "rbf-pum-direct",
+    ]:
+        for refinement in range(0, 12, 1):
+            # generate additional finer runs only for nn
+            if refinement > 9 and mapping != "nearest-neighbor":
+                continue
+
+            for basis_function in [
+                "compact-polynomial-c0",
+                "compact-polynomial-c2",
+                "compact-polynomial-c4",
+                "compact-polynomial-c6",
+                "compact-polynomial-c8",
+            ]:
+                path = script_path.parent / mapping
+                if mapping == "rbf-pum-direct":
+                    path = path / basis_function / str(support_radius)
+                path = path / f"refinement-{refinement}"
+
+                generate_run(
+                    path,
+                    refinement=refinement,
+                    mapping=mapping,
+                    basis_function=basis_function,
+                    support_radius=support_radius,
+                    constraint=constraint,
+                )
+
+                if mapping == "nearest-neighbor":
+                    # NN does not support different basis functions. Therefore,
+                    break
