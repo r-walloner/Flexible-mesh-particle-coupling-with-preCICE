e841dbd3af38a32c0be66e59d09c9113e801954b
Robin Walloner <wallonerrobin@gmail.com>
2025-04-04 17:41:32 +0200

    Update toy example to use JIT mapping

--- investigate_relativeReadTime/A/A.py
+++ investigate_relativeReadTime/A/A.py
@@ -4,20 +4,19 @@ participant = precice.Participant("A", "../precice-config.xml", 0, 1)
 
 mesh = [[0, 0]]
 vertex_ids = participant.set_mesh_vertices("A-Mesh", mesh)
-
 participant.initialize()
 
 t = 0
+dt = 1
 
 while participant.is_coupling_ongoing():
+    t = t + dt
     print("Timestep: ", t)
-    print("Generating data")
-    data = t
-    print("Generated data: ", data)
 
+    data = t
+    print("Writing data: ", data)
     participant.write_data("A-Mesh", "A-Data", vertex_ids, [data])
 
-    t = t + 1
-    participant.advance(1)
+    participant.advance(dt)
 
 participant.finalize()
--- investigate_relativeReadTime/B/B.py
+++ investigate_relativeReadTime/B/B.py
@@ -3,18 +3,23 @@ import precice
 participant = precice.Participant("B", "../precice-config.xml", 0, 1)
 
 participant.set_mesh_access_region("A-Mesh", [-1, 1, -1, 1])
-
 participant.initialize()
 
 t = 0
+dt = 1
 
 while participant.is_coupling_ongoing():
+    t = t + dt
     print("Timestep: ", t)
-    print("Receiving data")
-    data = participant.read_data("A-Mesh", "A-Data", [0], 0)
-    print("Received data: ", t)
 
-    t = t + 1
-    participant.advance(1)
+    relative_read_time = dt * 0
+    print("Relative read time: ", relative_read_time)
+    print("Absolute read time: ", t - dt + relative_read_time)
+
+    coordinates = [(0.0, 0.0)]
+    data = participant.map_and_read_data("A-Mesh", "A-Data", coordinates, 0)
+    print("Reading data: ", data)
+
+    participant.advance(dt)
 
 participant.finalize()
