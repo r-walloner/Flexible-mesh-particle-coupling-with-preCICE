f6956bc726c1603416ac50a300a459fc389a53ec
Robin Walloner <wallonerrobin@gmail.com>
2025-05-05 08:54:30 +0200

    Move precice-config figures to figures folder

--- particle_tracing_rayleigh_kothe_vortex/precice-config-graph.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-precice-config-visualizer \
- --data-access full ./precice-config.xml \
- --communicators hide \
- --cplschemes hide \
- --mappings full \
- > precice-config.dot \
-&& dot -Tpng -Gdpi=300 -O precice-config.png precice-config.dot
