d10b46c52394decbf4e6acd79bcafc9449fd5caa
Robin Walloner <wallonerrobin@gmail.com>
2025-06-13 16:58:38 +0200

    Improve flux plot

--- single_spout_bed/postprocessing/plot_particle_flux.py
+++ single_spout_bed/postprocessing/plot_particle_flux.py
@@ -11,15 +11,19 @@ with open(particle_path / "particle_flux.json", "r") as f:
 x_min = data['x_min']
 x_max = data['x_max']
 number_of_bins = data['number_of_bins']
+bins = np.linspace(0, x_max - x_min, number_of_bins)
+
 positive_flux = np.array(data['positive_flux'])
 negative_flux = np.array(data['negative_flux'])
 net_flux = np.array(data['net_flux'])
 
 plt.figure(figsize=(10, 6), dpi=600)
 plt.title("Average Particle Z-Flux at z=0.13")
+plt.xlim(0, x_max - x_min)
 plt.grid(True)
-plt.plot(np.linspace(x_min, x_max, number_of_bins), positive_flux, label='Flux in positive z-direction', color='red')
-plt.plot(np.linspace(x_min, x_max, number_of_bins), negative_flux, label='Flux in negative z-direction', color='blue')
-plt.plot(np.linspace(x_min, x_max, number_of_bins), net_flux, label='Net flux', linestyle='--', color='black')
+plt.xticks(np.linspace(0, x_max - x_min, 7))
+plt.plot(bins, positive_flux, label='Flux in positive z-direction', color='red')
+plt.plot(bins, negative_flux, label='Flux in negative z-direction', color='blue')
+plt.plot(bins, net_flux, label='Net flux', linestyle='--', color='black')
 plt.legend()
 plt.show()
