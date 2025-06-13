7b9a26492738f13a8352fc87a16d6d9cf9602e62
Robin Walloner <wallonerrobin@gmail.com>
2025-06-13 19:52:14 +0200

    Add reference data from Link 2005 to flux plot

--- single_spout_bed/postprocessing/plot_particle_flux.py
+++ single_spout_bed/postprocessing/plot_particle_flux.py
@@ -3,8 +3,15 @@ import pathlib
 import matplotlib.pyplot as plt
 import json
 
-particle_path = pathlib.Path(__file__).parent.parent / "particle-liggghts" / "post"
+script_dir = pathlib.Path(__file__).parent
 
+# Set up plot
+plt.figure(figsize=(10, 6), dpi=250)
+plt.title("Average Particle Z-Flux at z=0.13")
+
+
+# Load and plot flux data
+particle_path = script_dir.parent / "particle-liggghts" / "post"
 with open(particle_path / "particle_flux.json", "r") as f:
     data = json.load(f)
 
@@ -17,14 +24,19 @@ positive_flux = np.array(data['positive_flux'])
 negative_flux = np.array(data['negative_flux'])
 net_flux = np.array(data['net_flux'])
 
-plt.figure(figsize=(10, 6), dpi=250)
-plt.title("Average Particle Z-Flux at z=0.13")
 plt.xlim(0, x_max - x_min)
 plt.grid(True)
 plt.xticks(np.linspace(0, x_max - x_min, 7))
 plt.yticks(np.linspace(-800, 800, 9))
-plt.plot(bins, positive_flux, label='Flux in positive z-direction', color='red')
-plt.plot(bins, negative_flux, label='Flux in negative z-direction', color='blue')
+# plt.plot(bins, positive_flux, label='Flux in positive z-direction', color='red')
+# plt.plot(bins, negative_flux, label='Flux in negative z-direction', color='blue')
 plt.plot(bins, net_flux, label='Net flux', linestyle='--', color='black')
+
+# Load reference data
+reference_data_path = script_dir / "reference_data" / "Link_2005_experiment.csv"
+reference_data = np.loadtxt(reference_data_path, delimiter=",")
+plt.scatter(reference_data[:,1], reference_data[:,0], label='Link 2005', color='green')
+
+# Output plot
 plt.legend()
 plt.show()
