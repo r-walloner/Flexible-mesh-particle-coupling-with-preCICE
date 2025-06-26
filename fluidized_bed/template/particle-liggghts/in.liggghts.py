0f31c582360eaad34c4a3497a6ffcc94918596d4
Robin Walloner <wallonerrobin@gmail.com>
2025-06-26 20:41:30 +0200

    Fix write compression in template

--- fluidized_bed/template/particle-liggghts/in.liggghts.py
+++ fluidized_bed/template/particle-liggghts/in.liggghts.py
@@ -132,8 +132,8 @@ dump dmp all custom/vtk {int(p["output_interval"] / p["particle_dt"])} out/parti
     v_drag_coeff v_reynolds &
     radius v_volume v_alpha_p v_alpha_f
 {"""
-dump_modify dmp_settle binary yes
-dump_modify dmp_settle compressor lz4"""
+dump_modify dmp binary yes
+dump_modify dmp compressor lz4"""
  if p["output_compression"] else ""}
 
 
