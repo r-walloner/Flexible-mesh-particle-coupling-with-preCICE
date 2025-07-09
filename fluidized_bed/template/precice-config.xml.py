3c4bb8beae57a9d1b4c9e9a7536f0a28f007e3d7
Robin Walloner <wallonerrobin@gmail.com>
2025-07-09 18:24:49 +0200

    Optimize preCICE config (remove unused data)

--- fluidized_bed/template/precice-config.xml.py
+++ fluidized_bed/template/precice-config.xml.py
@@ -32,6 +32,55 @@ def generate(p):
             grain-dim="3"
             function-radius="{p["write_mapping_radius"]}" />
 """
+        
+    if p["coupling"] == "explicit":
+        data = """
+    <data:vector name="DragForce" />
+"""
+
+        mesh_use_data = """
+        <use-data name="DragForce" />
+"""
+
+        fluid_read_data = """
+        <read-data name="DragForce" mesh="Fluid-Mesh" />
+"""
+
+        particle_write_data = """
+        <write-data name="DragForce" mesh="Fluid-Mesh" />
+"""
+
+        exchange_data = """
+        <exchange data="DragForce" mesh="Fluid-Mesh" from="Particle" to="Fluid" />
+"""
+
+    
+
+    elif p["coupling"] == "semi_implicit":
+        data = """
+    <data:scalar name="ImplicitMomentum" />
+    <data:vector name="ExplicitMomentum" />
+"""
+
+        mesh_use_data = """
+        <use-data name="ImplicitMomentum" />
+        <use-data name="ExplicitMomentum" />
+"""
+
+        fluid_read_data = """
+        <read-data name="ImplicitMomentum" mesh="Fluid-Mesh" />
+        <read-data name="ExplicitMomentum" mesh="Fluid-Mesh" />
+"""
+
+        particle_write_data = """
+        <write-data name="ImplicitMomentum" mesh="Fluid-Mesh" />
+        <write-data name="ExplicitMomentum" mesh="Fluid-Mesh" />
+"""
+
+        exchange_data = """
+        <exchange data="ImplicitMomentum" mesh="Fluid-Mesh" from="Particle" to="Fluid" />
+        <exchange data="ExplicitMomentum" mesh="Fluid-Mesh" from="Particle" to="Fluid" />
+"""
 
     return f"""<?xml version="1.0" encoding="UTF-8"?>
 <precice-configuration experimental="True">
@@ -46,51 +95,41 @@ def generate(p):
     <profiling mode="{p["precice_profiling"]}" />
 
     <data:vector name="Velocity" />
-    <data:vector name="ExplicitMomentum" />
-    <data:scalar name="ImplicitMomentum" />
     <data:scalar name="Alpha" />
-    <data:vector name="DragForce" />
+    {data}
 
     <mesh name="Fluid-Mesh" dimensions="3">
         <use-data name="Velocity" />
-        <use-data name="ExplicitMomentum" />
-        <use-data name="ImplicitMomentum" />
         <use-data name="Alpha" />
-        <use-data name="DragForce" />
+        {mesh_use_data}
     </mesh>
 
     <participant name="Fluid">
         <provide-mesh name="Fluid-Mesh" />
         <write-data name="Velocity" mesh="Fluid-Mesh" />
-        <read-data name="ExplicitMomentum" mesh="Fluid-Mesh" />
-        <read-data name="ImplicitMomentum" mesh="Fluid-Mesh" />
         <read-data name="Alpha" mesh="Fluid-Mesh" />
-        <read-data name="DragForce" mesh="Fluid-Mesh" />
+        {fluid_read_data}
     </participant>
 
     <participant name="Particle">
         <receive-mesh name="Fluid-Mesh" from="Fluid" api-access="true" />
         <read-data name="Velocity" mesh="Fluid-Mesh" />
-        <write-data name="ExplicitMomentum" mesh="Fluid-Mesh" />
-        <write-data name="ImplicitMomentum" mesh="Fluid-Mesh" />
         <write-data name="Alpha" mesh="Fluid-Mesh" />
-        <write-data name="DragForce" mesh="Fluid-Mesh" />
+        {particle_write_data}
         {read_mapping}
         {write_mapping}
     </participant>
 
     <m2n:sockets acceptor="Fluid" connector="Particle" exchange-directory=".." />
 
-    <coupling-scheme:{p["coupling_scheme"]}>
+    <coupling-scheme:serial-explicit>
         <participants first="Fluid" second="Particle" />
         <time-window-size value="{p["fluid_dt"]}" />
         <max-time value="{p["end_time"]}" />
         <exchange data="Velocity" mesh="Fluid-Mesh" from="Fluid" to="Particle" />
-        <exchange data="ExplicitMomentum" mesh="Fluid-Mesh" from="Particle" to="Fluid" />
-        <exchange data="ImplicitMomentum" mesh="Fluid-Mesh" from="Particle" to="Fluid" />
         <exchange data="Alpha" mesh="Fluid-Mesh" from="Particle" to="Fluid" />
-        <exchange data="DragForce" mesh="Fluid-Mesh" from="Particle" to="Fluid" />
-    </coupling-scheme:{p["coupling_scheme"]}>
+        {exchange_data}
+    </coupling-scheme:serial-explicit>
 
 </precice-configuration>
 """
