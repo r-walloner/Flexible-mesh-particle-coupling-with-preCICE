a8bf6a8d1a5d19b5ac6c163328c94d2ccd237349
Robin Walloner <wallonerrobin@gmail.com>
2025-08-03 13:36:55 +0200

    Remove old fb flux postprocessing

--- fluidized_bed/plot_particle_flux.py
+++ fluidized_bed/plot_particle_flux.py
@@ -1,41 +1,106 @@
-import numpy as np
 import pathlib
+from sys import argv
 import matplotlib.pyplot as plt
-import json
+import numpy as np
 
 script_dir = pathlib.Path(__file__).parent.resolve()
-data_dir = script_dir / "data" / "particle_flux"
 reference_data_path = script_dir / "reference_data" / "Link 2005.csv"
 
-# Set up plot
-plt.figure(figsize=(10, 6), dpi=250)
-plt.title("Average Particle Z-Flux at z=0.13")
+bins = 30
+rho_p = 2505
+
+#Set up plot
+plt.figure(figsize=(10, 7))
+plt.rcParams.update(
+    {
+        "font.family": "serif",
+        "font.serif": ["Liberation Serif"],
+        "mathtext.fontset": "stix",
+        "font.size": 16,
+        "axes.labelsize": 16,
+        "axes.titlesize": 16,
+        "xtick.labelsize": 16,
+        "ytick.labelsize": 16,
+        "legend.fontsize": 16,
+        "xtick.color": "black",
+        "ytick.color": "black",
+        "axes.labelcolor": "black",
+    }
+)
+# plt.title("Settling velocity of a single particle")
 plt.grid(True)
+plt.xlabel("x [m]")
+plt.ylabel(r"Average particle z-flux [$\text{kg}/(\text{m}^2 \text{s})$]")
+plt.xlim(-0.075, 0.075)
+plt.ylim(-850, 850)
 
-xlim_max = None
 
-# Load and plot flux data
-for file in data_dir.glob("*.json"):
-    with open(file, "r") as f:
-        data = json.load(f)
+# Determine what runs to read
+if len(argv) > 1:
+    # If a runs are specified on the command line, use the data from those
+    runs = [pathlib.Path(r) for r in argv[1:]]
+else:
+    # Otherwise, use all runs in the runs directory
+    runs = list(script_dir.glob("runs/*"))
 
-    x_min = data['x_min']
-    x_max = data['x_max']
-    number_of_bins = data['number_of_bins']
-    bins = np.linspace(0, x_max - x_min, number_of_bins)
 
-    positive_flux = np.array(data['positive_flux'])
-    negative_flux = np.array(data['negative_flux'])
-    net_flux = np.array(data['net_flux'])
+for run in runs:
+    velocity_file = run / "particle-liggghts" / "out" / "average_velocity"
+    if not velocity_file.is_file():
+        print(f"Skipping {run.name} as it does not contain a velocity file")
+        continue
 
-    if xlim_max is None:
-        xlim_max = x_max - x_min
-    else:
-        xlim_max = max(x_max - x_min, xlim_max)
+    # Read in average velocity data
+    velocity: dict[int, list[float]] = {}
+    count: dict[int, list[float]] = {}
+    x_coords = []
+    with open(velocity_file, "r") as f:
+        lines = f.readlines()
 
-    # plt.plot(bins, positive_flux, label='Flux in positive z-direction', color='red')
-    # plt.plot(bins, negative_flux, label='Flux in negative z-direction', color='blue')
-    plt.plot(bins, net_flux, label=file.stem)
+        # Skip header
+        lines = lines[3:]
+
+        while lines:
+            line = lines.pop(0)
+            values = line.split(" ")
+            timestep = int(values[0])
+            velocity[timestep] = []
+            count[timestep] = []
+
+            for bin in range(bins):
+                line = lines.pop(0)
+                values = line.strip().split(" ")
+                velocity[timestep].append(float(values[3]))
+                count[timestep].append(float(values[2]))
+
+                # Only read x-coordinates from the first timestep
+                if len(x_coords) < bins:
+                    x_coords.append(float(values[1]))
+
+
+    # Calculate flux on each timestep
+    flux: dict[int, list[float]] = {}
+    for timestep in velocity.keys():
+        flux[timestep] = []
+        for bin in range(bins):
+            noramlized_count = count[timestep][bin] / (sum(count[timestep]) / bins)
+            flux[timestep].append(velocity[timestep][bin] * noramlized_count * rho_p)
+
+
+    # Average over timestep range and weight with count
+    flux_avg = [0] * bins
+
+    #averaging_range = velocity.keys()  # Use all timesteps
+    averaging_range = range(16100, 116101, 100) # Select specific range
+
+    for bin in range(bins):
+        for timestep in averaging_range:
+            flux_avg[bin] += flux[timestep][bin]
+        flux_avg[bin] /= len(averaging_range)
+
+    # Plot flux
+    plt.plot(x_coords, flux_avg, label=run.name)
+
 
 # Load and plot reference data
 reference_data = np.genfromtxt(reference_data_path, delimiter=",", skip_header=2)
@@ -43,6 +108,12 @@ link_exp = reference_data[:, 0:2][~np.isnan(reference_data[:, 0])]
 link_sim_min = reference_data[:, 2:4][~np.isnan(reference_data[:, 2])]
 link_sim_old = reference_data[:, 4:6][~np.isnan(reference_data[:, 4])]
 link_sim_koch = reference_data[:, 6:8][~np.isnan(reference_data[:, 6])]
+
+# Offset x-coordinates of reference data to match simulation
+link_exp[:, 0] -= 0.075
+link_sim_min[:, 0] -= 0.075
+link_sim_old[:, 0] -= 0.075
+link_sim_koch[:, 0] -= 0.075
 
 link_exp = link_exp[link_exp[:, 0].argsort()]
 link_sim_min = link_sim_min[link_sim_min[:, 0].argsort()]
@@ -54,9 +125,11 @@ plt.scatter(link_exp[:,0], link_exp[:,1], label='Link 2005 - exp', color="black"
 plt.plot(link_sim_old[:,0], link_sim_old[:,1], label='Link 2005 - sim - gidaspow', color="black", linestyle=":")
 plt.plot(link_sim_koch[:,0], link_sim_koch[:,1], label='Link 2005 - sim - koch', color="black", linestyle="--")
 
-# Output plot
-plt.xlim(0, xlim_max)
-plt.xticks(np.linspace(0, xlim_max, 7))
-plt.yticks(np.linspace(-800, 800, 9))
 plt.legend()
 plt.show()
+    
+
+                
+
+    
+
--- fluidized_bed/postprocess.py
+++ fluidized_bed/postprocess.py
@@ -12,109 +12,6 @@ runs_dir = script_dir / "runs"
 out_dir = script_dir / "data"
 
 
-# domain boundaries and measurement parameters
-# TODO get these from parameters.json (need to update generate.py first)
-x_min = -.075 # m
-x_max = +.075 # m
-y_min = -.0075 # m
-y_max = +.0075 # m
-plane_of_measurement = 0.13 # z-coordinate [m] of the plane where flux is measured
-number_of_bins = 32 # number of measurement bins along the x-axis
-
-
-# Extract the averaged particle flux across the measurement plane
-def extract_particle_flux(run_dir: pathlib.Path):
-    # Read run parameters
-    with open(run_dir / "parameters.json", "r") as f:
-        parameters = json.load(f)
-
-    # Check if output file already exists
-    output_file = out_dir / "particle_flux" / f"{run_dir.name}_5s.json"
-    if output_file.exists():
-        print(f"Particle flux {output_file.name} allready exists, skipping")
-        return
-    
-    print(f"Extracting particle flux for {run_dir.name}")
-
-    # Check if particle data exists
-    data_dir = run_dir / "particle-liggghts" / "out"
-    if not data_dir.exists():
-        print(f"{run_dir.name} does not contain data, skipping")
-        return
-
-    positive_flux = [int(0)] * number_of_bins
-    negative_flux = [int(0)] * number_of_bins
-
-    previous_positions: dict[int, np.array] = None
-
-    # Iterate over timesteps
-    timestep_files = data_dir.glob("particles_*.vtu")
-    timestep_files = sorted(timestep_files, key=lambda f: int(f.stem.split("_")[-1]))
-    timestep_files = timestep_files[:1000] # Only the first 1000 output files (5s @ output_interval 5e-3)
-    for timestep_file in tqdm(timestep_files):
-        timestep: pv.UnstructuredGrid = pv.read(timestep_file)
-
-        # Store particle positions by their IDs
-        positions_by_id: dict[int, np.array] = {}
-        for position, particle_id in zip(timestep.points, timestep.point_data["id"]):
-            positions_by_id[particle_id] = position
-
-        # Keep track of particle positions in the previous timestep
-        if previous_positions is None:
-            previous_positions = positions_by_id
-
-        # Iterate over all particles
-        for id, position in positions_by_id.items():
-            previous_position = previous_positions[id]
-
-            # Determine which bin the particle is in based on its x-coordinate
-            bin_index = int((position[0] - x_min) / (x_max - x_min) * number_of_bins)
-            if bin_index < 0 or bin_index >= number_of_bins:
-                print(f"Warning: Particle at {position[0]} is out of bounds for bins [{x_min}, {x_max}]")
-                continue
-            
-            # Check if the particle crossed the plane of measurement
-            if (position[1] > plane_of_measurement and previous_position[1] <= plane_of_measurement):
-                # Crossing in positive y-direction
-                positive_flux[bin_index] += 1
-            elif (position[1] < plane_of_measurement and previous_position[1] >= plane_of_measurement):
-                # Crossing in negative y-direction
-                negative_flux[bin_index] += 1
-
-        previous_positions = positions_by_id
-
-
-    # Normalize the flux by the total time and the area of the plane
-    bin_area = (x_max - x_min) * (y_max - y_min) / number_of_bins # area of each bin
-    positive_flux = np.array(positive_flux) / (5.0 * bin_area)
-    negative_flux = np.array(negative_flux) / (5.0 * bin_area)
-
-    # Multiply by particle mass
-    particle_volume = 4/3 * pi * (parameters["particle_diameter"] / 2)**3
-    particle_mass = particle_volume * parameters["particle_density"]
-    positive_flux *= particle_mass
-    negative_flux *= particle_mass
-    net_flux = positive_flux - negative_flux
-
-    # Write results to file
-    output_file.parent.mkdir(parents=True, exist_ok=True)
-    with open(output_file, 'w') as f:
-        json.dump({
-            'run_parameters': parameters,
-            'x_min': x_min,
-            'x_max': x_max,
-            'y_min': y_min,
-            'y_max': y_max,
-            'plane_of_measurement': plane_of_measurement,
-            'number_of_bins': number_of_bins,
-            'positive_flux': positive_flux.tolist(),
-            'negative_flux': negative_flux.tolist(),
-            'net_flux': net_flux.tolist(),
-        }, f, indent=4)
-
-    print()
-
-
 # Merge and extract profiling events
 def extract_profiling_events(run_dir: pathlib.Path):
     output_file = out_dir / "profiling" / f"{run_dir.name}.csv"
@@ -146,5 +43,4 @@ if __name__ == "__main__":
         runs = [pathlib.Path(arg) for arg in argv[1:]]
 
     for run_dir in runs:
-        extract_particle_flux(run_dir)
         # extract_profiling_events(run_dir)
